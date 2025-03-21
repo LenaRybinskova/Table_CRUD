c5abca5974a288c260ca87f24a0dd358ff88afef
«Elena <LenaRybinskova@gmail.com>
2025-03-21 13:11:55 +0300

    SCRUM-6: layout MUI

--- src/common/components/Input/InputContainer.tsx
+++ src/common/components/Input/InputContainer.tsx
@@ -11,7 +11,7 @@ export type InputContainerProps<T extends FieldValues> = TextFieldProps & {
     title?: string;
 };
 
-export const InputContainer = <T extends FieldValues>({name, control, errorMessage, rules = { required: 'Обязательное поле' }, textPlaceholder, title, ...rest}: InputContainerProps<T>) => {
+export const InputContainer = <T extends FieldValues>({name, control, errorMessage, rules = { required: 'Обязательное поле' }, textPlaceholder, autoComplete = "off", title, ...rest}: InputContainerProps<T>) => {
 
     return (
         <Controller
@@ -27,6 +27,7 @@ export const InputContainer = <T extends FieldValues>({name, control, errorMessa
                     error={!!errorMessage}
                     helperText={errorMessage || ""}
                     fullWidth
+                    autoComplete={autoComplete}
                 />
             )}
         />

7f1ceb9d0f813c231ffdd161f5121c8fe111cea0
«Elena <LenaRybinskova@gmail.com>
2025-03-21 11:02:13 +0300

    SCRUM-3:add loading, notification, handle error

--- src/common/components/Card/Card.tsx
+++ src/common/components/Card/Card.tsx
@@ -1,11 +1,29 @@
 import {Document, UpdateDate} from '@/features/TablePage/model/tableAPI.types.ts';
-import { Paper, Stack, Typography} from '@mui/material';
+import {Paper, Stack, Typography} from '@mui/material';
 import {useAppDispatch, useAppSelector} from '@/app/store.ts';
 import {deleteDocumentTC, updateDocumentTC} from '@/features/TablePage/model/tableReducer.ts';
 import {EditableSpan} from '@/common/components/EditSpan/EditSpan.tsx';
 import IconButton from '@mui/material/IconButton/IconButton';
 import {Delete} from '@mui/icons-material';
+import { styled } from '@mui/system';
 
+
+const StyledPaper = styled(Paper)({
+    position: "relative",
+    padding: 3,
+    maxWidth: 500,
+    minWidth: 400,
+    borderRadius: 2,
+    width: "100%",
+    height: "auto",
+});
+
+const StyledStack = styled(Stack)({
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+    alignItems: "flex-start",
+});
 
 type Props = {
     document: Document
@@ -33,32 +51,20 @@ export const Card = (props: Props) => {
         dispatch(updateDocumentTC(updatedate)).then()
     }
 
-
     return (
-        <Paper elevation={3} sx={{ padding: 2, maxWidth: 400, borderRadius: 2, position: "relative" }}>
-            <Stack spacing={1} alignItems="flex-start">
+        <StyledPaper elevation={3}>
+            <StyledStack>
                 {fields.map((field, index) => (
-                    <Typography
-                        component="span"
-                        key={`${document.id}${index}`}
-                        variant="body1"
-                        sx={{ display: "block", textAlign: "left" }}
-                    >
+                    <Typography component="span" key={`${document.id}${index}`} variant="body1" sx={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}>
                         <b>{field}:</b>
-                        <EditableSpan title={document[field]} onChange={editCardHandle} field={field}/>
+                        <EditableSpan title={document[field]} onChange={editCardHandle} field={field} />
                     </Typography>
                 ))}
-            </Stack>
-            <IconButton
-                onClick={handleDelete}
-                sx={{
-                    position: "absolute",
-                    top: 8,
-                    right: 8
-                }}
-            >
+            </StyledStack>
+
+            <IconButton onClick={handleDelete} sx={{ position: "absolute", top: 8, right: 8 }}>
                 <Delete />
             </IconButton>
-        </Paper>
+        </StyledPaper>
     )
 }
--- src/features/auth/ui/Auth.tsx
+++ src/features/auth/ui/Auth.tsx
@@ -3,8 +3,13 @@ import {useNavigate} from 'react-router-dom';
 import {validationRules} from '@/features/auth/ui/validationRules.ts';
 import {ButtonContainer} from '@/common/components/Button/ButtonContainer.tsx';
 import {InputContainer} from '@/common/components/Input/InputContainer.tsx';
-import {useAppDispatch} from '@/app/store.ts';
+import {useAppDispatch, useAppSelector} from '@/app/store.ts';
 import {loginTC} from '@/app/appReducer.ts';
+import {Stack} from '@mui/material';
+import {toast} from 'react-toastify';
+import {useEffect} from 'react';
+import {shallowEqual} from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 type FormValue = {
@@ -15,42 +20,54 @@ type FormValue = {
 //TODO интерсептор настроить
 export const Auth = () => {
     const {handleSubmit, control, formState: {errors, isValid}} = useForm<FormValue>({mode: 'onBlur'});
+    const error = useAppSelector(state => state.app.error, shallowEqual);
+
+
     const navigate = useNavigate();
     const dispatch = useAppDispatch()
 
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+        }
+    }, [error]);
+
     const onSubmit: SubmitHandler<FormValue> = (data: FormValue) => {
-        dispatch(loginTC(data)).then(res => {
-            if(res){
-                localStorage.setItem('username', data.username)
-                localStorage.setItem('token', res)
-            }
-            navigate('/table-page')
-        })
+        dispatch(loginTC(data))
+            .then(res => {
+                if (res) {
+                    localStorage.setItem('username', data.username)
+                    localStorage.setItem('token', res)
+                    navigate('/table-page')
+                }
+            })
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <InputContainer
-                name="username"
-                control={control}
-                errorMessage={errors.username?.message}
-                rules={validationRules.username}
-                textPlaceholder="Введите имя"
-                title="Имя пользователя"
-            />
+        <form onSubmit={handleSubmit(onSubmit)} style={{width: '100%'}}>
+            <Stack spacing={2}>
+                <InputContainer
+                    name="username"
+                    control={control}
+                    errorMessage={errors.username?.message}
+                    rules={validationRules.username}
+                    textPlaceholder="Введите имя"
+                    title="Имя пользователя"
+                />
 
-            <InputContainer
-                name="password"
-                control={control}
-                errorMessage={errors.password?.message}
-                textPlaceholder="Введите пароль"
-                title="Пароль"
-                type="password"
-            />
+                <InputContainer
+                    name="password"
+                    control={control}
+                    errorMessage={errors.password?.message}
+                    textPlaceholder="Введите пароль"
+                    title="Пароль"
+                    type="password"
+                />
 
-            <ButtonContainer type="submit" variant="contained" sx={{mt: 2}} disabled={!isValid}>
-                Отправить
-            </ButtonContainer>
+                <ButtonContainer type="submit" variant="contained" sx={{mt: 2}} disabled={!isValid}>
+                    Sign in
+                </ButtonContainer>
+            </Stack>
         </form>
     );
 }
